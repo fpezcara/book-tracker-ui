befde0a2afb44f1486a904375b4a86c8ecc57cbf
Florencia Pezcara <fpezcara@gmail.com>
2021-01-10 18:45:00 +0000

    necesito encontrar la manera que no se dispare la busqueda hasta que el usuario termino de escribir el termino...o sea submit

--- src/components/SearchBook.js
+++ src/components/SearchBook.js
@@ -1,17 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import useFetch from "../hooks/useFetch";
 
 const apiKey = process.env.REACT_APP_API_KEY;
-const urlTitle = process.env.REACT_APP_TITLE_URL
-console.log(urlTitle);
+const urlTitle = process.env.REACT_APP_TITLE_URL;
 
-const SearchBook = ({searchedTitle}) => {
-  const info = useFetch(
-    `${urlTitle}${searchedTitle}&download=epub&key=${apiKey}`
-  );
+const SearchBook = ({searchedTerm}) => {
+  //   const [data, setData] = useState([]);
 
-  console.log(info)
-  return <div>hola</div>
+   useFetch(
+    `${urlTitle}inauthor:${searchedTerm}&orderBy=relevance&key=${apiKey}`
+  )
+  // const searchedResult = () => {
+
+  // }
+
+  //   const searchByTitle = useFetch(
+  //     `${urlTitle}inauthor:${searchedTitle}&orderBy=relevance&key=${apiKey}`
+  //   );
+
+  //   console.log(searchByTitle);
+
+  //   const searchByAuthor = useFetch(
+  //     `${urlTitle}inauthor:${searchedTitle}&orderBy=relevance&key=${apiKey}`
+  //   );
+
+  //   const searchByISBN = useFetch(
+  //     `${urlTitle}isbn:${searchedTitle}&orderBy=relevance&key=${apiKey}`
+  //   );
+
+
 };
 
 export default SearchBook;
