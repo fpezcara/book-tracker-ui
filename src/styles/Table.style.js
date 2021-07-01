66a89fd8b9cb7ba11eaeebf6bfa356187e43abf8
florencia pezcara <fpezcara@gmail.com>
2021-07-01 18:49:48 +0100

    aligned table

--- src/styles/Table.style.js
+++ src/styles/Table.style.js
@@ -4,20 +4,15 @@ import { DeleteOutline } from "@styled-icons/material-rounded/DeleteOutline";
 export const TableContainer = styled.table`
   margin: 6em 2em;
   position: absolute;
-  left: 26em;
+  left: 23em;
   width: 60em;
   font-size: 14px;
   text-align: center;
   border-collapse: collapse;
 
-  tr {
-    /* margin-right: 0.2em; */
-  }
-
   th {
     background-color: #e3f2fd;
     border-radius: 0.1em;
-    margin-right: 0.2em;
     padding: 0.6em;
   }
   td {
