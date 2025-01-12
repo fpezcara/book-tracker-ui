f983ab9b6a20f00b3cbf70c2fde4c0d80cab4739
fpezcara <fpezcara@gmail.com>
2025-01-12 19:40:55 +0000

    Start writing App unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,22 +1,53 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
 import App from "./App";
 import { createMemoryHistory } from "history";
-import { Router } from "react-router-dom";
+import { MemoryRouter, Router } from "react-router-dom";
 import BookTrackerState from "./context/BookTrackerState";
 
 describe("App", () => {
-  const history = createMemoryHistory();
-  render(
-    <Router history={history}>
-      <BookTrackerState>
-        <App />
-      </BookTrackerState>
-    </Router>,
-  );
+  // test("redirects to /reading on first render", () => {
 
-  it("redirects to the right page on first render", () => {
-    const bookTrackerTitle = screen.getByText(/book tracker/i);
+  //   render(
+  //     <MemoryRouter>
+  //       <BookTrackerState>
+  //         <App />
+  //       </BookTrackerState>
+  //     </MemoryRouter>,
+  //   );
 
-    expect(bookTrackerTitle).toBeInTheDocument();
+  //   const bookTrackerTitle = screen.getByRole('heading', { level: 1 });
+  //   const bookList = screen.getByRole('heading', { level: 3 });
+  //   expect(bookTrackerTitle).toHaveTextContent(/book tracker/i);
+  //   expect(bookList).toHaveTextContent(/reading/i);
+  //   expect(screen.getByText(/no books have been added/i)).toBeInTheDocument();
+  // });
+
+  test("shows page not found when page does not exist", async () => {
+    const history = createMemoryHistory();
+
+    render(
+      <Router history={history}>
+        <BookTrackerState>
+          <App />
+        </BookTrackerState>
+      </Router>,
+    );
+    console.log("before");
+    screen.debug();
+
+    history.push("/random");
+
+    console.log("WHERE AM III", history.location);
+    console.log("after");
+
+    screen.debug();
+    await act(async () => {
+      // Wait for the 404 page to appear
+      const goHomeText = await screen.findByText(/go home/i);
+      expect(goHomeText).toBeInTheDocument();
+      expect(screen.getByAltText("page not found")).toBeInTheDocument();
+    });
+    // expect(screen.getByAltText('page not found')).toBeInTheDocument()
+    // expect(window.history.pathname).toBe('/404')
   });
 });
