69b6bac8dc0855a05e595a89bb559e879d8bb937
fpezcara <fpezcara@gmail.com>
2025-01-18 16:28:25 +0000

    Catch error when fetching Google API

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -5,9 +5,14 @@ const useFetch = (url) => {
   const [data, setData] = useState([]);
   useEffect(() => {
     const fetchApi = async () => {
-      const res = await axios.get(url);
-      const { data } = await res;
-      setData(data);
+      try {
+        const res = await axios.get(url);
+        const { data } = res;
+        setData(data);
+      } catch (error) {
+        console.error("Failed to fetch data:", error);
+        setData([]);
+      }
     };
     fetchApi();
   }, [url]);
