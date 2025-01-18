9abeb4cba403a96c0d49ae35a1efeac13fa81f05
fpezcara <fpezcara@gmail.com>
2025-01-18 16:28:31 +0000

    Add more tests for routes

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,5 @@
 import { render, screen, act } from "@testing-library/react";
-import { fireEvent } from "@testing-library/user-event";
-import App from "./App";
+import { userEvent } from "@testing-library/user-event";
 import { RouterProvider, createMemoryRouter } from "react-router-dom";
 import routesConfig from "./routesConfig";
 import BookTrackerState from "./context/BookTrackerState";
@@ -17,6 +16,7 @@ describe("App", () => {
     );
     const bookTrackerTitle = screen.getByRole("heading", { level: 1 });
     const bookList = screen.getByRole("heading", { level: 3 });
+
     expect(bookTrackerTitle).toHaveTextContent(/book tracker/i);
     expect(bookList).toHaveTextContent(/reading/i);
     expect(screen.getByText(/no books have been added/i)).toBeInTheDocument();
@@ -35,8 +35,27 @@ describe("App", () => {
 
     await act(async () => {
       const goHomeText = await screen.findByText(/go home/i);
+
       expect(goHomeText).toBeInTheDocument();
-      expect(screen.getByAltText("page not found")).toBeInTheDocument();
+      expect(screen.getByAltText("page not found")).toBeVisible();
     });
+  });
+
+  test("clicking on Add Book button navigates to /add-book", async () => {
+    const router = createMemoryRouter(routesConfig, {
+      initialEntries: ["/"],
+    });
+
+    render(
+      <BookTrackerState>
+        <RouterProvider router={router} />
+      </BookTrackerState>,
+    );
+
+    userEvent.click(await screen.findByText("Add book"));
+    console.log(await screen.findByText("Add book"));
+    expect(
+      await screen.findByText("search by Title, Author or ISBN"),
+    ).toBeInTheDocument();
   });
 });
