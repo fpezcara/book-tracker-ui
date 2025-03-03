a3d50c1b3e9a795060d82348130cf636edfb984b
fpezcara <fpezcara@gmail.com>
2025-03-03 20:54:34 +0000

    Add links to login and register in Header and styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Nav } from "../../styles/Header.style";
 
 const Header = () => {
   return (
     <Nav>
-      <h1>Book Tracker</h1>
+      <div className="authentication">
+        <Link className="link" to="/register">
+          Register
+        </Link>
+        <Link className="link" to="/login">
+          Login
+        </Link>
+      </div>
+      <div>
+        <h1>Book Tracker</h1>
+      </div>
     </Nav>
   );
 };
