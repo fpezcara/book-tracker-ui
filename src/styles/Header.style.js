34505e62ffa668299fd00b232674ae2a37e00f5a
fpezcara <fpezcara@gmail.com>
2025-03-03 20:54:34 +0000

    Add links to login and register in Header and styles

--- src/styles/Header.style.js
+++ src/styles/Header.style.js
@@ -5,7 +5,8 @@ export const Nav = styled.header`
   justify-content: center;
   align-items: center;
   flex-wrap: wrap;
-  padding: 2.5em 0 0.8em 0;
+  padding: 0.5rem 0.5em 0;
+  flex-direction: column;
 
   h1 {
     font-family: "Lato", sans-serif;
@@ -18,6 +19,71 @@ export const Nav = styled.header`
       -1px 3px 1px #f6ffff,
       -2px 5px 1px #fffffe;
     text-align: center;
+  }
+
+  div {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+  }
+
+  .authentication {
+    gap: 2rem;
+    justify-content: flex-end;
+=  }
+
+  .link {
+    appearance: none;
+    background-color: transparent;
+    border: 2px solid #1a1a1a;
+    border-radius: 15px;
+    box-sizing: border-box;
+    color: #3b3b3b;
+    cursor: pointer;
+    display: inline-block;
+    font-family:
+      Roobert,
+      -apple-system,
+      BlinkMacSystemFont,
+      "Segoe UI",
+      Helvetica,
+      Arial,
+      sans-serif,
+      "Apple Color Emoji",
+      "Segoe UI Emoji",
+      "Segoe UI Symbol";
+    font-size: 16px;
+    font-weight: 600;
+    line-height: normal;
+    margin: 0;
+    min-height: 40px;
+    min-width: 0;
+    outline: none;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
+    transition: all 300ms cubic-bezier(0.23, 1, 0.32, 1);
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+    width: 8rem;
+    will-change: transform;
+  }
+
+  .link:disabled {
+    pointer-events: none;
+  }
+
+  .link:hover {
+    color: #fff;
+    background-color: #1a1a1a;
+    box-shadow: rgba(0, 0, 0, 0.25) 0 8px 15px;
+    transform: translateY(-2px);
+  }
+
+  .link:active {
+    box-shadow: none;
+    transform: translateY(0);
   }
 
   @media only screen and (max-width: 768px) {
