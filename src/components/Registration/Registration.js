8cda0104e476faaaa28f9277df79ae0dacef6965
fpezcara <fpezcara@gmail.com>
2025-03-04 00:50:53 +0000

    Update more Registration styles

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { AuthenticationContainer } from "../../styles/Authentication.style";
+
 const Registration = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -12,18 +15,21 @@ const Registration = () => {
     console.log(formValues);
   };
   return (
-    <section>
-      <form onSubmit={handleSubmit}>
-        <input type="email" name="email" placeholder="Email address" />
-        <input type="password" name="password" placeholder="Password" />
-        <input
-          type="password"
-          name="password-confirmation"
-          placeholder="Password Confirmation"
-        />
-        <button type="submit">Register</button>
-      </form>
-    </section>
+    <AuthenticationContainer>
+      <div className="wrapper">
+        <h2>Register</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="email" name="email" placeholder="Email address" />
+          <input type="password" name="password" placeholder="Password" />
+          <input
+            type="password"
+            name="password-confirmation"
+            placeholder="Password Confirmation"
+          />
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    </AuthenticationContainer>
   );
 };
 
