2599214eb62794c8f7d3a4e34425affd33fe2a3c
fpezcara <fpezcara@gmail.com>
2025-03-09 21:20:25 +0000

    Add API call to Registration form

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,4 +1,7 @@
 import React from "react";
+import axios from "axios";
+import { API_URL } from "../../constants";
+
 import { AuthenticationContainer } from "../../styles/Authentication.style";
 
 const Registration = () => {
@@ -7,12 +10,22 @@ const Registration = () => {
     const formData = new FormData(e.target);
 
     const formValues = {
-      email: formData.get("email"),
+      email_address: formData.get("email"),
       password: formData.get("password"),
       passwordConfirmation: formData.get("password-confirmation"),
     };
 
-    console.log(formValues);
+    axios
+      .post(`${API_URL}/users`, {
+        user: formValues,
+      })
+      .then((response) => {
+        // todo : i can't see the cookie, but i need to store that so i can make reqeusts to the api
+        console.log(response.json());
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
   return (
     <AuthenticationContainer>
