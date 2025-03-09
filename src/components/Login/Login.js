19e808a3f97a9247695e88842ba036312304fa43
fpezcara <fpezcara@gmail.com>
2025-03-09 21:18:58 +0000

    Add Login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,3 +1,56 @@
-const Login = () => <p>Login</p>;
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthenticationContainer } from "../../styles/Authentication.style";
+import { API_URL } from "../../constants";
+
+import axios from "axios";
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const formValues = {
+      email_address: formData.get("email"),
+      password: formData.get("password"),
+    };
+
+    axios
+      .post(
+        `${API_URL}/session`,
+        {
+          ...formValues,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      )
+      .then((response) => {
+        if (response.status === 200 || response.status === 204) {
+          navigate("/");
+        }
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <AuthenticationContainer>
+      <div className="wrapper">
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="email" name="email" placeholder="Email address" />
+          <input type="password" name="password" placeholder="Password" />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </AuthenticationContainer>
+  );
+};
 
 export default Login;
