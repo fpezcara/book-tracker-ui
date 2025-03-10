5339905fb669f91a95d3fe09cc2adfaf1fbb2b13
fpezcara <fpezcara@gmail.com>
2025-03-10 19:07:51 +0000

    Re-add REACT_APP before prod env variable

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? process.env.BOOK_TRACKER_API_PRODUCTION
+    ? process.env.REACT_APP_BOOK_TRACKER_API_PRODUCTION
     : process.env.REACT_APP_BOOK_TRACKER_API;
