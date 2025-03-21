2f222ef33fa830a7c86df235ca031a2a04e153aa
fpezcara <fpezcara@gmail.com>
2025-03-21 21:29:50 +0000

    Update to react-router v7

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import React from "react";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router";
 import BookTrackerState from "./context/BookTrackerState";
-import routesConfig from "./routesConfig";
+import routes from "./routesConfig";
 
 const App = () => {
-  const router = createBrowserRouter(routesConfig);
+  const router = createBrowserRouter(routes, {
+    future: {
+      v7_relativeSplatPath: true,
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+    },
+  });
 
   return (
     <BookTrackerState>
-      <RouterProvider router={router} />
+      <RouterProvider
+        router={router}
+        future={{
+          v7_startTransition: true,
+        }}
+      />
     </BookTrackerState>
   );
 };
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen, act } from "@testing-library/react";
 import { userEvent } from "@testing-library/user-event";
-import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import { RouterProvider, createMemoryRouter } from "react-router";
 import routesConfig from "./routesConfig";
 import BookTrackerState from "./context/BookTrackerState";
 
