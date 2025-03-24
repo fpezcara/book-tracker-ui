a00eac8fcbd0e95b392a1283dd33f3ec328d1ec2
fpezcara <fpezcara@gmail.com>
2025-03-24 23:06:44 +0000

    Redirect user to login if not authenticated

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,21 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { useParams, Navigate } from "react-router";
 
 import HomeHeader from "./Header/index.jsx";
 import Table from "./Table/index.jsx";
 import BookTrackerContext from "../../context/book-tracker-context.js";
 import LoadingSpinner from "../LoadingSpinner";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router";
 
 import { HomeContainer } from "../../styles/Home.style.js";
 
 const Home = () => {
   const { name } = useParams();
+  const userId = Cookies.get("userId");
+  const navigate = useNavigate();
+
   const {
     loading,
     state: { bookLists },
   } = useContext(BookTrackerContext);
 
   const linkMatches = bookLists.some((item) => item.listUrl === name);
+
+  useEffect(() => {
+    if (!userId) {
+      navigate("/login");
+    }
+  }, [userId]);
 
   return (
     <HomeContainer data-testid="home-container">
