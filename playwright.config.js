df81152c7c867c692968f60f4fd2b415489d209d
fpezcara <fpezcara@gmail.com>
2025-04-28 22:52:10 +0100

    Add retries

--- playwright.config.js
+++ playwright.config.js
@@ -11,7 +11,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
 
   // Retry on CI only.
-  retries: process.env.CI ? 2 : 0,
+  // retries: process.env.CI ? 2 : 0,
+  retries: 3,
 
   // Opt out of parallel tests on CI.
   workers: process.env.CI ? 1 : undefined,
