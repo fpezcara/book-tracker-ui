0f9d5707226ff97695ecc3a159fc7d61dcd343ef
fpezcara <fpezcara@gmail.com>
2025-07-27 23:04:16 -0300

    Add further testing for ConfirmationModal

--- src/components/ConfirmationModal/ConfirmationModal.test.js
+++ src/components/ConfirmationModal/ConfirmationModal.test.js
@@ -1,4 +1,6 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+
 import ConfirmationModal from ".";
 import BookTrackerProvider from "../../context/BookTrackerProvider";
 import { MemoryRouter, Route, Routes } from "react-router";
@@ -6,62 +8,117 @@ import Cookies from "js-cookie";
 
 import { mockBookLists } from "../../../test/mocks/bookListsMock";
 import { bookMock } from "../../../test/mocks/bookMock";
+import { addBookToList, removeBookFromList } from "../../utils/requests";
+
+jest.mock("../../utils/requests", () => ({
+  addBookToList: jest.fn(),
+  removeBookFromList: jest.fn(),
+}));
+
+const renderComponent = ({ isVisible = false, message }) => {
+  Cookies.set("userId", mockBookLists[0].user_id);
+  Cookies.set("currentBookList", mockBookLists[0].name.toLowerCase());
+  fetch.mockResponse(JSON.stringify(mockBookLists));
+
+  return render(
+    <BookTrackerProvider value={{ lists: mockBookLists }}>
+      <MemoryRouter initialEntries={["/reading"]}>
+        <Routes>
+          <Route
+            path="/:name"
+            element={
+              <ConfirmationModal
+                isVisible={isVisible}
+                hideModal={jest.fn()}
+                message={message}
+                book={bookMock}
+              />
+            }
+          />
+        </Routes>
+      </MemoryRouter>
+    </BookTrackerProvider>,
+  );
+};
 
 describe("ConfirmationModal", () => {
-  const hideModalMock = jest.fn();
+  beforeEach(() => {
+    fetch.resetMocks();
+  });
 
-  test("When isVisible is set to false, it does not display the modal", () => {
-    const { queryByTestId } = render(
-      <BookTrackerProvider>
-        <ConfirmationModal
-          hideModal={hideModalMock}
-          message="delete"
-          book={bookMock}
-        />
-      </BookTrackerProvider>,
-    );
+  test("When isVisible is set to false, it does not display the modal", async () => {
+    const { queryByTestId } = renderComponent({ message: "add" });
 
-    const modal = queryByTestId("confirmation-modal");
-
-    expect(modal).toBeFalsy();
+    await waitFor(() => {
+      expect(queryByTestId("confirmation-modal")).toBeFalsy();
+    });
   });
 
   describe("When isVisible is set to true", () => {
-    beforeEach(() => {
-      Cookies.set("userId", "9");
-      Cookies.set("currentBookList", "reading");
-      fetch.mockResponse(JSON.stringify(mockBookLists));
-    });
-
     it.each([
       ["add", "Do you want to add:"],
       ["delete", "Do you want to delete:"],
     ])(
       "and the command is to %s, the message displayed should be: %s",
       (str, expectedText) => {
-        const { queryByTestId, getByText } = render(
-          <BookTrackerProvider value={{ lists: mockBookLists }}>
-            <MemoryRouter initialEntries={["/reading"]}>
-              <Routes>
-                <Route
-                  path="/:name"
-                  element={
-                    <ConfirmationModal
-                      isVisible={true}
-                      hideModal={jest.fn()}
-                      message={str}
-                      book={bookMock}
-                    />
-                  }
-                />
-              </Routes>
-            </MemoryRouter>
-          </BookTrackerProvider>,
-        );
+        const { queryByTestId, getByText } = renderComponent({
+          isVisible: true,
+          message: str,
+        });
 
-        expect(queryByTestId("confirmation-modal")).toBeTruthy();
-        expect(getByText(expectedText)).toBeTruthy();
+        waitFor(() => {
+          expect(queryByTestId("confirmation-modal")).toBeTruthy();
+          expect(getByText(expectedText)).toBeTruthy();
+        });
       },
     );
+  });
+
+  describe('When the message is "add"', () => {
+    it("it calls addBookToList with the correct arguments", async () => {
+      const { getByTestId } = renderComponent({
+        isVisible: true,
+        message: "add",
+      });
+
+      fetch.mockResponseOnce(JSON.stringify({ book: bookMock }));
+      const acceptButton = await getByTestId(
+        "confirmation-modal-accept-button",
+      );
+
+      await userEvent.click(acceptButton);
+
+      waitFor(() => {
+        expect(addBookToList).toHaveBeenCalledWith(
+          mockBookLists[0].user_id.toString(),
+          mockBookLists[0].id,
+          bookMock,
+        );
+      });
+    });
+  });
+
+  describe('When the message is "delete"', () => {
+    it("it calls removeBookToList with the correct arguments", async () => {
+      const { findByTestId } = renderComponent({
+        isVisible: true,
+        message: "delete",
+      });
+
+      fetch.mockResponseOnce(JSON.stringify({ book_id: bookMock.id }));
+      const acceptButton = await findByTestId(
+        "confirmation-modal-accept-button",
+      );
+
+      await userEvent.click(acceptButton);
+
+      waitFor(() => {
+        expect(removeBookFromList).toHaveBeenCalledWith(
+          mockBookLists[0].user_id.toString(),
+          mockBookLists[0].id,
+          bookMock.id,
+        );
+      });
+    });
   });
 });
